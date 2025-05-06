12bf17d05db499aa7d92fd83827ac8005ffa3b24
snutt10 <snutt@student.neumont.edu>
2025-05-06 14:08:56 -0600

    add comments

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int main() {
+    // '\n' and 'endl' both go to the next line
     cout << "Hello, World!" << endl;
+    cout << "Git is now tracking this project!" << endl;
     return 0;
 }
